48982a17e2b6a5ab031670e8d823e96749d1b106
Safiullah Korai <shahzaibth3gr8@gmail.com>
2024-03-20 19:10:42 +0000

    committing the commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Shahzaib The Great",
+  title: "Web Designer & Graphic",
   email: "alex@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
+  twitter: "Shahzaib",
   youTube: "Code",
 };
 
